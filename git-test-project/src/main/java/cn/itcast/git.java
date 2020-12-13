4aa5885b87dacfac4255d0f36d03a5b157a1a824
强魏东 <641770222@qq.com>
2020-12-13 21:38:58 +0800

    更新了一个方法，测试的方法

--- git-test-project/src/main/java/cn/itcast/git.java
+++ git-test-project/src/main/java/cn/itcast/git.java
@@ -6,8 +6,9 @@ public class git {
         System.out.println("hello git ！！！");
         System.out.println("hello git ！！！");
         System.out.println("hello git ！！！");
+        test();
     }
-    public void test(){
+    public static void test(){
         int i = 10;
         System.out.println(i*10);
     }
