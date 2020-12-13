7d2a29fa31e03b8e649106153bce0bffda839c3c
强魏东 <641770222@qq.com>
2020-12-13 21:13:34 +0800

    把本地工程提交的git

---
+++ git-test-project/src/main/java/cn/itcast/git.java
@@ -0,0 +1,10 @@
+package cn.itcast;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello git ！！！");
+        System.out.println("hello git ！！！");
+        System.out.println("hello git ！！！");
+        System.out.println("hello git ！！！");
+    }
+}
